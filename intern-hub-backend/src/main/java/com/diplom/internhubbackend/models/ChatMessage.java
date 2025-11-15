48db9342a96e8cc1418bc42a3d23ed9066860988
GabberSlam <egor20033084@gmail.com>
2025-11-15 03:28:02 +0300

    Изменена модель сообщения

--- intern-hub-backend/src/main/java/com/diplom/internhubbackend/models/ChatMessage.java
+++ intern-hub-backend/src/main/java/com/diplom/internhubbackend/models/ChatMessage.java
@@ -2,38 +2,25 @@ package com.diplom.internhubbackend.models;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Date;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-
-@Entity
-@Table(name="chat_messages")
+@Document(collection = "chat_message")
 public class ChatMessage {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Getter
-    @Setter
-    private Long id;
-    @Getter
-    @Setter
+    private String id;
     private String chatId;
-    @Getter
-    @Setter
     private String senderId;
-    @Getter
-    @Setter
     private String recipientId;
     private String senderName;
     private String recipientName;
-    private String message;
-    @Temporal(TemporalType.TIMESTAMP)
+    private String content;
     private Date timestamp;
-    @Enumerated(EnumType.STRING)
     private MessageStatus status;
-
 
     public enum MessageStatus {
         RECEIVED, DELIVERED
