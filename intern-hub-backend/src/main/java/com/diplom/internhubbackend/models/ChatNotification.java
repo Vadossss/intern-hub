2689dc88993221bd502d24e7a9b1ef61e2a688b6
GabberSlam <egor20033084@gmail.com>
2025-11-15 03:28:43 +0300

    Изменена модель уведомления о сообщении

--- intern-hub-backend/src/main/java/com/diplom/internhubbackend/models/ChatNotification.java
+++ intern-hub-backend/src/main/java/com/diplom/internhubbackend/models/ChatNotification.java
@@ -9,19 +9,16 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.relational.core.mapping.Table;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-
-@Entity
-@Table(name = "notifications")
+@Document(collection = "chat_notification")
 public class ChatNotification {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private String id;
     private String senderId;
     private String senderName;
 }
