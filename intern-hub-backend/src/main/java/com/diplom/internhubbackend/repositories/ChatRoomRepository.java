7c35647f43029e9751c80d5b9d69c906645b2e17
GabberSlam <egor20033084@gmail.com>
2025-11-15 03:30:10 +0300

    Изменен репозиторий чата, теперь он для Mongo

--- intern-hub-backend/src/main/java/com/diplom/internhubbackend/repositories/ChatRoomRepository.java
+++ intern-hub-backend/src/main/java/com/diplom/internhubbackend/repositories/ChatRoomRepository.java
@@ -2,9 +2,11 @@ package com.diplom.internhubbackend.repositories;
 
 import com.diplom.internhubbackend.models.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.Optional;
 
-public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
-    Optional<ChatRoom> findBySenderIdAndRecipientId(String senderId, String recepientId);
+
+public interface ChatRoomRepository extends MongoRepository<ChatRoom, String> {
+    Optional<ChatRoom> findBySenderIdAndRecipientId(String senderId, String recipientId);
 }
