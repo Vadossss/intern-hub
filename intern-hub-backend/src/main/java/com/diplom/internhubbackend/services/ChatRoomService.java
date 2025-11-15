16b6d1185ff85ef12aada72ca00b8760c41cf106
GabberSlam <egor20033084@gmail.com>
2025-11-15 03:33:08 +0300

    Изменен сервис для чата

--- intern-hub-backend/src/main/java/com/diplom/internhubbackend/services/ChatRoomService.java
+++ intern-hub-backend/src/main/java/com/diplom/internhubbackend/services/ChatRoomService.java
@@ -3,35 +3,47 @@ package com.diplom.internhubbackend.services;
 
 import com.diplom.internhubbackend.models.ChatRoom;
 import com.diplom.internhubbackend.repositories.ChatRoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
 public class ChatRoomService {
-    private final ChatRoomRepository chatRoomRepository;
 
-    public ChatRoomService(ChatRoomRepository chatRoomRepository) {
-        this.chatRoomRepository = chatRoomRepository;
+    @Autowired
+    private ChatRoomRepository chatRoomRepository;
+
+    public Optional<String> getChatId(
+            String senderId, String recipientId, boolean createIfNotExist) {
+
+        return chatRoomRepository
+                .findBySenderIdAndRecipientId(senderId, recipientId)
+                .map(ChatRoom::getChatId)
+                .or(() -> {
+                    if(!createIfNotExist) {
+                        return  Optional.empty();
+                    }
+                    var chatId =
+                            String.format("%s_%s", senderId, recipientId);
+
+                    ChatRoom senderRecipient = ChatRoom
+                            .builder()
+                            .chatId(chatId)
+                            .senderId(senderId)
+                            .recipientId(recipientId)
+                            .build();
+
+                    ChatRoom recipientSender = ChatRoom
+                            .builder()
+                            .chatId(chatId)
+                            .senderId(recipientId)
+                            .recipientId(senderId)
+                            .build();
+                    chatRoomRepository.save(senderRecipient);
+                    chatRoomRepository.save(recipientSender);
+
+                    return Optional.of(chatId);
+                });
     }
-
-    // Создание новой комнаты (если не существует)
-    public ChatRoom createOrGetChatRoom(String senderId, String recipientId) {
-        Optional<ChatRoom> existingRoom = chatRoomRepository.findBySenderIdAndRecipientId(senderId, recipientId);
-        return existingRoom.orElseGet(() -> {
-            ChatRoom room = new ChatRoom();
-            room.setSenderId(senderId);
-            room.setRecipientId(recipientId);
-            return chatRoomRepository.save(room);
-        });
-    }
-
-    // Получить комнату по ID
-    public Optional<ChatRoom> getChatRoomById(Long id ) {
-        return chatRoomRepository.findById(id );
-    }
-
-
-
-
 }
