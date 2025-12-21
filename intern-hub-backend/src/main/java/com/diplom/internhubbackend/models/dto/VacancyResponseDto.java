c10a8a7ed34f7b44155e0aa91e3b1da9bbdd8a20
Vadossss <iv.gav789@mail.ru>
2025-12-21 05:34:36 +0300

    Добавлена dto для вакансии ответа

---
+++ intern-hub-backend/src/main/java/com/diplom/internhubbackend/models/dto/VacancyResponseDto.java
@@ -0,0 +1,27 @@
+package com.diplom.internhubbackend.models.dto;
+
+import com.diplom.internhubbackend.models.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.util.Set;
+
+@Getter
+@Setter
+public class VacancyResponseDto {
+    private Integer id;
+    private String title;
+    private String description;
+    private String city;
+    private Integer salaryFrom;
+    private Integer salaryTo;
+    private Currency currency;
+    private String charge;
+    private String requirements;
+    private String conditions;
+    private String link;
+    private Employment employment;
+    private Experience experience;
+    private WorkFormat workFormat;
+    private User employer;
+    private Set<KeySkillDto> skills;
+}
