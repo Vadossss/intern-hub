b09c58301f94ed0d9e43bef340bbcf47cd7e92b7
Vadossss <iv.gav789@mail.ru>
2025-12-22 02:57:22 +0300

    Обновлен репозиторий вакансий

--- intern-hub-backend/src/main/java/com/diplom/internhubbackend/repositories/VacancyRepository.java
+++ intern-hub-backend/src/main/java/com/diplom/internhubbackend/repositories/VacancyRepository.java
@@ -1,10 +1,13 @@
 package com.diplom.internhubbackend.repositories;
 
+import com.diplom.internhubbackend.models.Stack;
 import com.diplom.internhubbackend.models.Vacancy;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface VacancyRepository extends JpaRepository<Vacancy, Integer> {
-//    @Query("SELECT v FROM Vacancy v WHERE v.id = :id JOIN FETCH ")
-//    Vacancy fullFindById(Integer id);
+    @Query("SELECT v FROM Vacancy v WHERE v.stack = :stack")
+    List<Vacancy> findByStack(Stack stack);
 }
