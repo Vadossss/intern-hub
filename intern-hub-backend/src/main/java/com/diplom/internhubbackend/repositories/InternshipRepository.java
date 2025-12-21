2eccc723be1b349376ea350d0cb06a1b33e1be20
Vadossss <iv.gav789@mail.ru>
2025-12-21 05:14:54 +0300

    Рефакторинг

--- intern-hub-backend/src/main/java/com/diplom/internhubbackend/repositories/InternshipRepository.java
+++
@@ -1,7 +0,0 @@
-package com.diplom.internhubbackend.repositories;
-
-import com.diplom.internhubbackend.models.Internship;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InternshipRepository extends JpaRepository<Internship, Integer> {
-}
