5754b2b5b52b30076db2ebbe392bea7c45a735d2
Vadossss <iv.gav789@mail.ru>
2025-12-23 22:39:33 +0300

    Обновлён сервис авторизации

--- intern-hub-backend/src/main/java/com/diplom/internhubbackend/services/AuthService.java
+++ intern-hub-backend/src/main/java/com/diplom/internhubbackend/services/AuthService.java
@@ -20,12 +20,14 @@ import java.time.Duration;
 @Service
 public class AuthService {
     private final UserRepository userRepository;
+    private final AuthUtilService authUtilService;
     private final JwtUtil jwtUtil;
     private final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
     private final CustomUserDetailsService customUserDetailsService;
 
-    public AuthService(UserRepository userRepository, JwtUtil jwtUtil, CustomUserDetailsService customUserDetailsService) {
+    public AuthService(UserRepository userRepository, AuthUtilService authUtilService, JwtUtil jwtUtil, CustomUserDetailsService customUserDetailsService) {
         this.userRepository = userRepository;
+        this.authUtilService = authUtilService;
         this.jwtUtil = jwtUtil;
         this.customUserDetailsService = customUserDetailsService;
     }
@@ -34,11 +36,11 @@ public class AuthService {
         if (user.getEmail() != null && existsByEmail(user.getEmail())) {
             throw new EmailAlreadyExistsException("Email already exists");
         }
-        else {
-            if (user.getPhoneNumber() != null && existsByPhoneNumber(user.getPhoneNumber())) {
-                throw new PhoneNumberAlreadyExistsException("Phone Number already exists");
-            }
-        }
+//        else {
+//            if (user.getPhoneNumber() != null && existsByPhoneNumber(user.getPhoneNumber())) {
+//                throw new PhoneNumberAlreadyExistsException("Phone Number already exists");
+//            }
+//        }
 
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         user.setRole(UserRole.STUDENT);
@@ -49,32 +51,53 @@ public class AuthService {
         if (customUserDetails == null) {
             throw new TokenGenerationException("Failed to load user details for token generation");
         }
-        return generateAuthResponse(customUserDetailsService.loadUserByUsername(user.getEmail()));
+        return authUtilService.generateAuthResponse(customUserDetailsService.loadUserByUsername(user.getEmail()));
     }
 
     public TokensCookieDto loginUser(UserRegisterDto userRegisterDto) {
         String email = userRegisterDto.getEmail();
-        String phone = userRegisterDto.getPhoneNumber();
 
-        if (email == null && phone == null) {
+        if (email == null) {
             throw new UserNotFoundException("Email or phone number is incorrect");
         }
 
         User user;
-        if (email != null && existsByEmail(email)) {
+        if (existsByEmail(email)) {
             user = userRepository.findByEmail(email)
                     .orElseThrow(() -> new UserNotFoundException(email));
-        } else if (phone != null && existsByPhoneNumber(phone)) {
-            user = userRepository.findByPhoneNumber(phone)
-                    .orElseThrow(() -> new UserNotFoundException(phone));
         } else {
             throw new UserNotFoundException("Email or phone number is incorrect");
         }
 
-        if (passwordMatches(userRegisterDto.getPassword(), user.getPassword())) {
-            return generateAuthResponse(customUserDetailsService.loadUserByUsername(user.getEmail()));
+        if (authUtilService.passwordMatches(userRegisterDto.getPassword(), user.getPassword())) {
+            return authUtilService.generateAuthResponse(customUserDetailsService.loadUserByUsername(user.getEmail()));
         } else {
             throw new PasswordIncorrectException("Incorrect password");
+        }
+    }
+
+    public Boolean validateToken(HttpServletRequest request) {
+        String accessToken = null;
+        if (request.getCookies() != null) {
+            for (var cookie : request.getCookies()) {
+                if ("accessToken".equals(cookie.getName())) {
+                    accessToken = cookie.getValue();
+                    break;
+                }
+            }
+        }
+
+        if (accessToken == null) {
+            throw new RefreshTokenException("Access token not found");
+        }
+
+        CustomUserDetails customUserDetails = jwtUtil.parseJwtToken(accessToken);
+
+        if (customUserDetails != null) {
+            return true;
+        }
+        else {
+            throw new RefreshTokenException("Access token expired");
         }
     }
 
@@ -96,14 +119,14 @@ public class AuthService {
         CustomUserDetails customUserDetails = jwtUtil.parseJwtToken(refreshToken);
 
         if (customUserDetails != null) {
-            return generateAuthResponse(customUserDetails);
+            return authUtilService.generateAuthResponse(customUserDetails);
         }
         else {
             throw new RefreshTokenException("Refresh token expired");
         }
     }
 
-    private TokensCookieDto generateAuthResponse(CustomUserDetails customUserDetails) {
+    public TokensCookieDto generateAuthResponse(CustomUserDetails customUserDetails) {
         JwtPair tokens = jwtUtil.generateTokenPair(customUserDetails);
         ResponseCookie refreshTokenCookie = ResponseCookie.from("refreshToken", tokens.getRefreshToken())
                 .httpOnly(true)
@@ -139,7 +162,7 @@ public class AuthService {
                 .orElseThrow(() -> new UserNotFoundException("User not found"));
     }
 
-    public boolean passwordMatches(String password, String hashedPassword) {
-        return passwordEncoder.matches(password, hashedPassword);
-    }
+//    public boolean passwordMatches(String password, String hashedPassword) {
+//        return passwordEncoder.matches(password, hashedPassword);
+//    }
 }
