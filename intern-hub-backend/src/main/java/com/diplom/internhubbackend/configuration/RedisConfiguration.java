a3ca7a2c3d904a7c7196c2858083cc48b81268ab
Vadossss <iv.gav789@mail.ru>
2026-03-03 14:51:59 +0300

    Обновлена конфигурация Redis

--- intern-hub-backend/src/main/java/com/diplom/internhubbackend/configuration/RedisConfiguration.java
+++ intern-hub-backend/src/main/java/com/diplom/internhubbackend/configuration/RedisConfiguration.java
@@ -2,9 +2,6 @@ package com.diplom.internhubbackend.configuration;
 
 import com.diplom.internhubbackend.models.VacancyCache;
 import com.diplom.internhubbackend.properties.RedisKeysProperties;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -12,15 +9,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisKeyValueAdapter;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.convert.KeyspaceConfiguration;
-import org.springframework.data.redis.core.convert.MappingConfiguration;
-import org.springframework.data.redis.core.index.IndexConfiguration;
-import org.springframework.data.redis.core.mapping.RedisMappingContext;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-
-import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
@@ -30,50 +21,23 @@ public class RedisConfiguration {
     private final RedisKeysProperties properties;
 
     @Bean
-    public RedisTemplate<String, VacancyCache> internshipRedisTemplate(
-            RedisConnectionFactory factory,
-            ObjectMapper objectMapper
+    public RedisTemplate<String, VacancyCache> redisTemplate(
+            RedisConnectionFactory factory
     ) {
         RedisTemplate<String, VacancyCache> template = new RedisTemplate<>();
         template.setConnectionFactory(factory);
 
-        Jackson2JsonRedisSerializer<VacancyCache> serializer =
-                new Jackson2JsonRedisSerializer<>(objectMapper, VacancyCache.class);
 
         template.setKeySerializer(new StringRedisSerializer());
         template.setHashKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(serializer);
-        template.setHashValueSerializer(serializer);
+
+        GenericJackson2JsonRedisSerializer jsonSerializer =
+                new GenericJackson2JsonRedisSerializer();
+
+        template.setValueSerializer(jsonSerializer);
+        template.setHashValueSerializer(jsonSerializer);
 
         template.afterPropertiesSet();
         return template;
-    }
-
-    @Bean
-    public RedisMappingContext keyValueMappingContext() {
-        return new RedisMappingContext(
-                new MappingConfiguration(new IndexConfiguration(), new CustomKeyspaceConfiguration()));
-    }
-
-    public class CustomKeyspaceConfiguration extends KeyspaceConfiguration {
-
-        @Override
-        protected Iterable<KeyspaceSettings> initialConfiguration() {
-            return List.of(
-                    customKeyspaceSettings(VacancyCache.class, CacheName.VACANCY, properties.
-                            getVacancy().getTimeToLive().toSeconds())
-            );
-        }
-
-        private <T> KeyspaceSettings customKeyspaceSettings(Class<T> type, String keyspace, long ttlSeconds) {
-            KeyspaceSettings settings = new KeyspaceSettings(type, keyspace);
-            settings.setTimeToLive(ttlSeconds);
-            return settings;
-        }
-    }
-
-    @NoArgsConstructor(access = AccessLevel.PRIVATE)
-    public static class CacheName {
-        public static final String VACANCY = "vacancy";
     }
 }
--- intern-hub-backend/src/main/java/com/diplom/internhubbackend/configuration/RedisSearchInitializer.java
+++ intern-hub-backend/src/main/java/com/diplom/internhubbackend/configuration/RedisSearchInitializer.java
@@ -6,21 +6,21 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
-@Slf4j
-@Component
-@RequiredArgsConstructor
+//@Slf4j
+//@Component
+//@RequiredArgsConstructor
 public class RedisSearchInitializer {
 
-    private final VacanciesCacheService vacanciesCacheService;
+//    private final VacanciesCacheService vacanciesCacheService;
 
-    @PostConstruct
-    public void init() {
-        try {
-            log.info("Инициализация RediSearch...");
-            vacanciesCacheService.initSearchIndex();
-            log.info("RediSearch инициализирован успешно");
-        } catch (Exception e) {
-            log.warn("Не удалось инициализировать RediSearch. Будет использована обычная фильтрация.", e);
-        }
-    }
+//    @PostConstruct
+//    public void init() {
+//        try {
+//            log.info("Инициализация RediSearch...");
+//            vacanciesCacheService.initSearchIndex();
+//            log.info("RediSearch инициализирован успешно");
+//        } catch (Exception e) {
+//            log.warn("Не удалось инициализировать RediSearch. Будет использована обычная фильтрация.", e);
+//        }
+//    }
 }
