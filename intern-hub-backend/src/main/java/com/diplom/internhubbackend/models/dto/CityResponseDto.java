8d1d88f161f1e64e2da741a59c60003970e133ad
Vadossss <iv.gav789@mail.ru>
2026-03-03 14:48:39 +0300

    Добавлена dto для статистики по количеству вакансий по городам

---
+++ intern-hub-backend/src/main/java/com/diplom/internhubbackend/models/dto/CityResponseDto.java
@@ -0,0 +1,17 @@
+package com.diplom.internhubbackend.models.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class CityResponseDto {
+    private String cityName;
+    private Integer count;
+
+    public CityResponseDto(String cityName) {
+        this.cityName = cityName;
+    }
+}
