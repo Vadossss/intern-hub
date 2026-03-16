5cfe7a1ae04a9570319d8305bf2199057af6de9d
Vadossss <iv.gav789@mail.ru>
2026-03-16 22:27:32 +0300

    Удалены устаревшие классы и репозитории

--- intern-hub-backend/src/main/java/com/diplom/internhubbackend/repositories/EducationOrganizationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.diplom.internhubbackend.repositories;
-
-import com.diplom.internhubbackend.models.EducationOrganization;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EducationOrganizationRepository extends JpaRepository<EducationOrganization, Long> {
-}
