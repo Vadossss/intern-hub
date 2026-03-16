750c7975103aa7ab06bee1bf35e0a7ca349f219a
Vadossss <iv.gav789@mail.ru>
2026-03-16 23:06:37 +0300

    Создано dto для параметров фильтрации вакансий

---
+++ intern-hub-backend/src/main/java/com/diplom/internhubbackend/dto/FilterParamsRequest.java
@@ -0,0 +1,59 @@
+package com.diplom.internhubbackend.dto;
+
+import com.diplom.internhubbackend.enums.PositionsEnum;
+import com.diplom.internhubbackend.enums.VacancySourceCode;
+import com.diplom.internhubbackend.enums.VacancyStatus;
+import io.swagger.v3.oas.annotations.Parameter;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+public class FilterParamsRequest {
+    @Parameter(description = "Источник вакансии")
+    private List<VacancySourceCode> source;
+
+    @Enumerated(EnumType.STRING)
+    @Parameter(description = "Позиция/должность")
+    private PositionsEnum position;
+
+    @Parameter(description = "Город")
+    private String city;
+
+    @Parameter(description = "График работы")
+    private String schedule;
+
+    @Parameter(description = "Тип занятости")
+    private String employment;
+
+    @Parameter(description = "Минимальная зарплата")
+    private Long salaryMin;
+
+    @Parameter(description = "Максимальная зарплата")
+    private Long salaryMax;
+
+    @Parameter(description = "Статус вакансии (Активна/На проверке/Архивирована)")
+    private VacancyStatus status;
+
+    @Parameter(description = "Текст для полнотекстового поиска")
+    private String searchText;
+
+    @Parameter(description = "Формат работы")
+    private List<String> workFormats;
+
+    @Parameter(description = "Номер страницы (начиная с 0)")
+    private Integer page = 0;
+
+    @Parameter(description = "Размер страницы")
+    private Integer size = 20;
+
+    @Parameter(description = "Поле для сортировки")
+    private String sortBy = "name";
+
+    @Parameter(description = "Направление сортировки (asc/desc)")
+    private String sortDirection = "asc";
+}
