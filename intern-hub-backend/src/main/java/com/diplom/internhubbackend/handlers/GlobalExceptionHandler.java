d9c49f3dd7f5cf961cf3d54863786ae670854ea2
Vadossss <iv.gav789@mail.ru>
2026-03-16 23:48:41 +0300

    Обновлён обработчик исключений

--- intern-hub-backend/src/main/java/com/diplom/internhubbackend/handlers/GlobalExceptionHandler.java
+++ intern-hub-backend/src/main/java/com/diplom/internhubbackend/handlers/GlobalExceptionHandler.java
@@ -9,6 +9,11 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
     public class GlobalExceptionHandler {
 
+    @ExceptionHandler(VacancyNotFoundException.class)
+    public ResponseEntity<Object> handleVacancyExists(VacancyNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
     @ExceptionHandler(EmailAlreadyExistsException.class)
     public ResponseEntity<Object> handleEmailExists(EmailAlreadyExistsException ex) {
         return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
@@ -26,7 +31,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
     @ExceptionHandler(RefreshTokenException.class)
     public ResponseEntity<Object> handleRefreshTokenException(RefreshTokenException ex) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ex.getMessage());
     }
 
     @ExceptionHandler(TokenGenerationException.class)
@@ -36,9 +41,9 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
                 .body(ex.getMessage());
     }
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<Object> handleGeneral(Exception ex) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body("Internal error: " + ex.getMessage());
-    }
+//    @ExceptionHandler(Exception.class)
+//    public ResponseEntity<Object> handleGeneral(Exception ex) {
+//        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+//                .body("Internal error: " + ex.getMessage());
+//    }
 }
