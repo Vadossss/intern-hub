02a7d5c74ca6784d3dd007d5b33e9f77fb8124fd
Vadossss <iv.gav789@mail.ru>
2026-03-17 00:07:00 +0300

    Обновлен контроллер вакансий

--- intern-hub-backend/src/main/java/com/diplom/internhubbackend/controllers/VacancyController.java
+++ intern-hub-backend/src/main/java/com/diplom/internhubbackend/controllers/VacancyController.java
@@ -1,21 +1,20 @@
 package com.diplom.internhubbackend.controllers;
 
+import com.diplom.internhubbackend.dto.*;
+import com.diplom.internhubbackend.mapper.FilterParamsMapper;
 import com.diplom.internhubbackend.mapper.VacancyMapper;
-import com.diplom.internhubbackend.models.VacancyCache;
-import com.diplom.internhubbackend.models.dto.CityResponseDto;
-import com.diplom.internhubbackend.models.dto.FilterParams;
-import com.diplom.internhubbackend.models.dto.NewVacancyDto;
-import com.diplom.internhubbackend.models.dto.PageResponse;
-import com.diplom.internhubbackend.models.enums.VacancySource;
-import com.diplom.internhubbackend.services.CustomUserDetailsService;
+import com.diplom.internhubbackend.enums.PositionsEnum;
+import com.diplom.internhubbackend.enums.VacancySourceCode;
+import com.diplom.internhubbackend.enums.VacancyStatus;
+import com.diplom.internhubbackend.security.config.CustomUserDetails;
 import com.diplom.internhubbackend.services.VacancyService;
-import com.diplom.internhubbackend.services.VacanciesCacheService;
 import io.swagger.v3.oas.annotations.Operation;
-import jakarta.annotation.security.DeclareRoles;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,43 +22,40 @@ import java.util.List;
 @Slf4j
 @RestController
 @RequestMapping("/vacancy")
+@RequiredArgsConstructor
 public class VacancyController {
 
     private final VacancyService vacancyService;
-    private final VacanciesCacheService vacanciesCacheService;
     private final VacancyMapper vacancyMapper;
-    private final CustomUserDetailsService customUserDetailsService;
-
-    public VacancyController(VacancyService vacancyService, VacanciesCacheService vacanciesCacheService,
-                             VacancyMapper vacancyMapper, CustomUserDetailsService customUserDetailsService) {
-        this.vacancyService = vacancyService;
-        this.vacanciesCacheService = vacanciesCacheService;
-        this.vacancyMapper = vacancyMapper;
-        this.customUserDetailsService = customUserDetailsService;
-    }
+    private final FilterParamsMapper filterParamsMapper;
 
     @PreAuthorize("hasAuthority('ROLE_EMPLOYER')")
-    @PostMapping("/createVacancy")
-    public ResponseEntity<Object> createInternship(@RequestBody NewVacancyDto vacancyRequest) {
-        log.info(customUserDetailsService.getCurrentUser().getRole().getId());
-        return vacancyService.createVacancy(vacancyMapper.fromDto(vacancyRequest));
+    @PostMapping()
+    @Operation(summary = "Создание вакансии")
+    public ResponseEntity<Object> createInternship(
+            @AuthenticationPrincipal CustomUserDetails customUserDetails,
+            @RequestBody NewVacancyDto vacancyRequest
+    ) {
+        return vacancyService.createVacancy(customUserDetails.getUser(), vacancyRequest);
     }
 
-    @GetMapping("/getVacancies")
-    public ResponseEntity<Object> getVacancy(@RequestParam Integer id) {
-        return ResponseEntity.ok(vacancyMapper.toDto(vacancyService.getVacancy(id)));
+    @GetMapping("/{vacancyId}")
+    @Operation(summary = "Получение вакансии по id")
+    public ResponseEntity<Object> getVacancy(@PathVariable String vacancyId) {
+        return ResponseEntity.ok(vacancyMapper.toDto(vacancyService.getVacancy(vacancyId)));
     }
 
     @GetMapping
-    @Operation(summary = "Расширенный поиск вакансий с использованием RediSearch")
-    public ResponseEntity<PageResponse<VacancyCache>> searchVacancies(
-            @RequestParam(required = false) VacancySource source,
-            @RequestParam(required = false) String position,
+    @Operation(summary = "Расширенный поиск вакансий")
+    public ResponseEntity<PageResponse<VacancyResponseDto>> searchVacancies(
+            @RequestParam(required = false) List<VacancySourceCode> source,
+            @RequestParam(required = false) PositionsEnum position,
             @RequestParam(required = false) String city,
             @RequestParam(required = false) String schedule,
             @RequestParam(required = false) String employment,
-            @RequestParam(required = false) String salaryMin,
-            @RequestParam(required = false) String salaryMax,
+            @RequestParam(required = false) Long salaryMin,
+            @RequestParam(required = false) Long salaryMax,
+            @RequestParam(required = false) VacancyStatus status,
             @RequestParam(required = false) String searchText,
             @RequestParam(required = false) List<String> workFormats,
             @RequestParam(required = false, defaultValue = "0") Integer page,
@@ -67,7 +63,7 @@ public class VacancyController {
             @RequestParam(required = false, defaultValue = "name") String sortBy,
             @RequestParam(required = false, defaultValue = "asc") String sortDirection) {
 
-        FilterParams filterParams = new FilterParams();
+        FilterParamsRequest filterParams = new FilterParamsRequest();
         filterParams.setSource(source);
         filterParams.setPosition(position);
         filterParams.setCity(city);
@@ -75,6 +71,7 @@ public class VacancyController {
         filterParams.setEmployment(employment);
         filterParams.setSalaryMin(salaryMin);
         filterParams.setSalaryMax(salaryMax);
+        filterParams.setStatus(status);
         filterParams.setSearchText(searchText);
         filterParams.setWorkFormats(workFormats);
         filterParams.setPage(page);
@@ -82,45 +79,33 @@ public class VacancyController {
         filterParams.setSortBy(sortBy);
         filterParams.setSortDirection(sortDirection);
 
-        List<VacancyCache> results = vacanciesCacheService.searchWithRediSearch(filterParams);
-        log.info("Вакансий: " + results.size());
+        Page<VacancyResponseDto> results =
+                vacancyService.getVacanciesByParams(filterParamsMapper.toDto(filterParams));
 
-        int fromIndex = filterParams.getPage() != null ? filterParams.getPage() * filterParams.getSize() : 0;
-        int pageSize = filterParams.getSize() != null ? filterParams.getSize() : results.size();
 
-        if (fromIndex >= results.size()) {
-            return ResponseEntity.ok(PageResponse.of(
-                    List.of(),
-                    filterParams.getPage() != null ? filterParams.getPage() : 0,
-                    pageSize,
-                    results.size()
-            ));
-        }
-
-        int toIndex = Math.min(fromIndex + pageSize, results.size());
-        List<VacancyCache> pageContent = results.subList(fromIndex, toIndex);
-
-        return ResponseEntity.ok(PageResponse.of(
-                pageContent,
-                filterParams.getPage() != null ? filterParams.getPage() : 0,
-                pageSize,
-                results.size()
-        ));
+        return ResponseEntity.ok(
+                PageResponse.of(
+                        results.getContent(),
+                        results.getNumber(),
+                        results.getSize(),
+                        results.getTotalElements()
+                )
+        );
     }
-
-    @GetMapping("/search/text")
-    @Operation(summary = "Полнотекстовый поиск по всем полям")
-    public ResponseEntity<List<VacancyCache>> fullTextSearch(
-            @RequestParam String query) {
-
-        List<VacancyCache> results = vacanciesCacheService.fullTextSearch(query);
-        return ResponseEntity.ok(results);
-    }
-
-    @GetMapping("/cities")
-    @Operation(summary = "Получить список уникальных городов")
-    public ResponseEntity<List<String>> getCities() {
-        List<String> cities = vacanciesCacheService.getDistinctCities();
-        return ResponseEntity.ok(cities);
-    }
+//
+//    @GetMapping("/search/text")
+//    @Operation(summary = "Полнотекстовый поиск по всем полям")
+//    public ResponseEntity<List<VacancyCache>> fullTextSearch(
+//            @RequestParam String query) {
+//
+//        List<VacancyCache> results = vacanciesCacheService.fullTextSearch(query);
+//        return ResponseEntity.ok(results);
+//    }
+//
+//    @GetMapping("/cities")
+//    @Operation(summary = "Получить список уникальных городов")
+//    public ResponseEntity<List<String>> getCities() {
+//        List<String> cities = vacanciesCacheService.getDistinctCities();
+//        return ResponseEntity.ok(cities);
+//    }
 }
