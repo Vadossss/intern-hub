7c7f454ff88a491cc312d35e5b414000ac773202
Vadossss <iv.gav789@mail.ru>
2026-03-16 22:58:52 +0300

    Создан класс для отклика

---
+++ intern-hub-backend/src/main/java/com/diplom/internhubbackend/models/Application.java
@@ -0,0 +1,51 @@
+package com.diplom.internhubbackend.models;
+
+import com.diplom.internhubbackend.enums.ContactMethod;
+import com.diplom.internhubbackend.enums.VacancyApplicationStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(
+        name = "application",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"vacancy_id", "candidate_id"})
+        })
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Application {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "vacancy_id", nullable = false)
+    private Vacancy vacancy;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "candidate_id", nullable = false)
+    private User candidate;
+
+    @Column(columnDefinition = "TEXT")
+    private String coverLetter;
+
+    @Column(name = "status")
+    @Enumerated(EnumType.STRING)
+    @Builder.Default
+    private VacancyApplicationStatus status = VacancyApplicationStatus.PENDING;
+
+    @Enumerated(EnumType.STRING)
+    private ContactMethod chosenContactMethod;
+
+    @Column(name = "created_at")
+    @Builder.Default
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
