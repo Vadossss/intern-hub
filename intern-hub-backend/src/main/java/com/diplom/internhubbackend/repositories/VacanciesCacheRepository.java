d9a19ec489f242ca34e08d910f240494d756115e
Vadossss <iv.gav789@mail.ru>
2026-03-16 23:19:24 +0300

    Удалены устаревший класс и репозиторий

--- intern-hub-backend/src/main/java/com/diplom/internhubbackend/repositories/VacanciesCacheRepository.java
+++
@@ -1,8 +0,0 @@
-package com.diplom.internhubbackend.repositories;
-
-import com.diplom.internhubbackend.models.VacancyCache;
-import org.springframework.data.repository.CrudRepository;
-
-public interface VacanciesCacheRepository extends CrudRepository<VacancyCache, String> {
-
-}
