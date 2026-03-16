959feb18d076b2be5ce75fa820d09a8561c21a57
Vadossss <iv.gav789@mail.ru>
2026-03-16 23:47:15 +0300

    Обновлён кастомный user details

--- intern-hub-backend/src/main/java/com/diplom/internhubbackend/security/config/CustomUserDetails.java
+++ intern-hub-backend/src/main/java/com/diplom/internhubbackend/security/config/CustomUserDetails.java
@@ -1,6 +1,7 @@
 package com.diplom.internhubbackend.security.config;
 
 import com.diplom.internhubbackend.models.User;
+import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -8,6 +9,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 import java.util.List;
 
+@Getter
 public class CustomUserDetails implements UserDetails {
 
     private User user;
