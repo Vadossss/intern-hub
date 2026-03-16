62945cf9be49552696ab214fde1fc484faee5588
Vadossss <iv.gav789@mail.ru>
2026-03-16 23:54:31 +0300

    Обновлен конфиг кэша

--- intern-hub-backend/src/main/java/com/diplom/internhubbackend/configuration/CacheConfig.java
+++ intern-hub-backend/src/main/java/com/diplom/internhubbackend/configuration/CacheConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 public class CacheConfig {
     @Bean
     public CacheManager cacheManager() {
-        return new ConcurrentMapCacheManager("vacancies");
+        return new ConcurrentMapCacheManager("role", "stacks", "workFormat",
+                "keySkills", "experience", "employment", "vacancySource", "currency", "vacancy");
     }
 }
