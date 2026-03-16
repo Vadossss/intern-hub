434bf5b7cb767dda4d7086ebd36fc138728833b1
Vadossss <iv.gav789@mail.ru>
2026-03-16 23:07:27 +0300

    Создан класс для избранных вакансий

---
+++ intern-hub-backend/src/main/java/com/diplom/internhubbackend/models/FavoriteVacancy.java
@@ -0,0 +1,30 @@
+package com.diplom.internhubbackend.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "favorite_vacancy")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class FavoriteVacancy {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "vacancy_id")
+    private Vacancy vacancy;
+
+    @Builder.Default
+    private LocalDateTime addedAt = LocalDateTime.now();
+}
