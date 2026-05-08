e6a05fe3e636ff08ea7cf99abe436c67a97caf45
Vadossss <iv.gav789@mail.ru>
2026-05-08 14:51:15 +0300

    Реализованы страницы соискателя и работодателя

---
+++ intern-hub-frontend/app/candidates/[id]/page.tsx
@@ -0,0 +1,5 @@
+import { CandidatePublicPage } from "@/components/shared/candidate/CandidatePublicPage";
+
+export default function CandidatePage() {
+  return <CandidatePublicPage />;
+}
---
+++ intern-hub-frontend/app/employers/[id]/page.tsx
@@ -0,0 +1,5 @@
+import { EmployerPublicPage } from "@/components/shared/employer/EmployerPublicPage";
+
+export default function EmployerPage() {
+  return <EmployerPublicPage />;
+}
